a39020fd672dbd435287156e8c3aa2e1b7f63838
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-09-20 18:26:05 +0800

    update CSS,JS for header, progress-bar animation

--- js/main.js
+++ js/main.js
@@ -17,6 +17,34 @@ const swiper = new Swiper('.swiper', {
    
   });
 
+  $(document).ready(function () {
+    function headerImage() {
+      let scrollValue = $(window).scrollTop(); 
+      if (scrollValue > 40 ){
+        $('.header').find('img').height('60px');
+        // console.log(scrollValue);
+      }
+      else{
+        $('.header').find('img').height('80px');
+      }
+    }
+  
+    function progressBar(){
+      let scrollValue = $(window).scrollTop();
+      if (scrollValue > 550){
+        $('.item-progress li:nth-child(1)').find('.progress-bar').width('35%');
+        $('.item-progress li:nth-child(2)').find('.progress-bar').width('55%');
+        $('.item-progress li:nth-child(3)').find('.progress-bar').width('75%');
+      } 
+    }
+
+    $(window).scroll(function(){
+      headerImage();
+      progressBar()
+    });
+
+    
+  });
 
 
   
